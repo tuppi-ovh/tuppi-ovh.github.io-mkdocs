f81243a694c3be67fb525ce156d7753a9cdb3b04
tuppi-ovh <tuppi.ovh@gmail.com>
2021-05-21 22:01:12 +0200

    Reogranization

--- publish.sh
+++ publish.sh
@@ -1,17 +1,11 @@
 #!/bin/bash
 
-# prepare sub projects
-git clone https://github.com/tuppi-ovh/data-server-pi.git github/data-server-pi
-git clone https://github.com/tuppi-ovh/data-server-stm32.git github/data-server-stm32
-git clone https://github.com/tuppi-ovh/data-server-vmc.git github/data-server-vmc
-git clone https://github.com/tuppi-ovh/tuppi-ovh.github.io.git github/tuppi-ovh.github.io
-
 # generate a site 
 mkdir temp
 python3 -m mkdocs build -d temp
 
 # upload
-cd github/tuppi-ovh.github.io
+cd ext/tuppi-ovh.github.io
 git pull
 cp -rf ../../temp/* .
 git add .
@@ -20,4 +14,3 @@ git push
 
 # clean up & exit
 rm -rf ../../temp
-rm -rf github/*
